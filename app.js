98a9e9308e8711a0b0f3e58f641b1736eb0eeb54
Harsh Jain <harshjain5454@gmail.com>
2021-12-25 18:13:30 +0530

    feat::sparkles:create compose route and page

--- app.js
+++ app.js
@@ -30,6 +30,14 @@ app.get("/contact", (req, res) => {
     });
 });
 
+app.get("/compose", (req, res) => {
+    res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+    console.log(req.body.content);
+});
+
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
